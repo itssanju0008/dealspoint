b8fc30376d17889fd33759687d3df225c428f07b
sanjeev singh <sanju682295@gmail.com>
2024-11-21 21:12:42 +0530

    updated

--- src/models&Routes/Videos/Videos.model.js
+++ src/models&Routes/Videos/Videos.model.js
@@ -37,8 +37,20 @@ const load = {
   },
   votes: {
     type: Number,
-    default: () => Math.floor(Math.random() * 9000) + 1000, // Random number between 1000 and 9999
+    default: function () {
+      return Math.floor(Math.random() * (this.views - 1000)) + 1000; // Less than views
+    },
     min: 1000,
+    validate: {
+      validator: function (value) {
+        return value < this.views;
+      },
+      message: "Votes must be less than views.",
+    },
+  },
+  dislikes: {
+    type: Number,
+    default: 1,
   },
 };
 
